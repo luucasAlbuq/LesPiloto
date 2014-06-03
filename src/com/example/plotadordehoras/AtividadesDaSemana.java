0be92ae0d067d103ae1e885e85e9d83a1c8a1e39
pherivelton <pherivelton@gmail.com>
2014-06-03 14:45:35 -0300

    Finalizei o Layout dos Relatorios

--- src/com/example/plotadordehoras/AtividadesDaSemana.java
+++ src/com/example/plotadordehoras/AtividadesDaSemana.java
@@ -1,6 +1,7 @@
 package com.example.plotadordehoras;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -8,9 +9,15 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+/**
+ * 
+ * @author Pablo
+ *
+ * Classe que Exibe o Relatorio das Atividades para o usuário
+ * 
+ */
+
 public class AtividadesDaSemana extends Activity{
-	
-	private Button geraRelatorio;
 	
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +26,9 @@ public class AtividadesDaSemana extends Activity{
         
         Spinner mesAno = (Spinner) findViewById(R.id.mesesAno);
         Spinner semanasMes = (Spinner) findViewById(R.id.semanaMes);
-        geraRelatorio = (Button) findViewById(R.id.geraRelatorioAtividade);
+        Button geraRelatorio = (Button) findViewById(R.id.botaoGeraRelatorioAtividade);
+        Button geraHistorico = (Button) findViewById(R.id.botaoHistoricoComparativo);
+        Button voltarRelatorio = (Button) findViewById(R.id.botaoVoltar);
         
         String[] meses = getResources().getStringArray(R.array.meses_ano);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, meses);
@@ -31,12 +40,46 @@ public class AtividadesDaSemana extends Activity{
 		arrayAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_item);
 		semanasMes.setAdapter(arrayAdapter2);
 		
+		/**
+		 * Gera o relatório das atividades da semana atual para que o usuario
+		 * possa ver como o tempo investido está distribuido nas atividades
+		 */
+		
 		geraRelatorio.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				
 				//Lucas põe o código aqui....
+			}
+		});
+		
+		/**
+		 * Exibe o Historico da Semana ATUAL com as 2 semanas anteriores
+		 */
+		
+		geraHistorico.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				
+				//Lucas Põe o Código aqui
+				
+			}
+		});
+		
+		/**
+		 * Volta para a Activity principal
+		 */
+		
+		voltarRelatorio.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				Intent telaPrincipal = new Intent(AtividadesDaSemana.this, MainActivity.class);
+				AtividadesDaSemana.this.startActivity(telaPrincipal);
+				
 			}
 		});
 	}
