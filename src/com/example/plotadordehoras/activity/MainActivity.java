af94176a26b44a11797fc2daee92d8af4616ef45
Tales Tenorio de Souza Pimentel <talestsp@users.noreply.github.com>
2014-06-25 20:44:18 -0300

    Comitando mudanças aqui por problemas de git push

--- src/com/example/plotadordehoras/activity/MainActivity.java
+++ src/com/example/plotadordehoras/activity/MainActivity.java
@@ -1,78 +1,49 @@
 package com.example.plotadordehoras.activity;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
+import java.util.List;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import model.Atividade;
+import model.Mes;
+import android.content.Context;
 
-import com.example.plotadordehoras.R;
+import com.projetopiloto.plotadordehoras.util.GerarRelatorioDaSemana;
 
-public class MainActivity extends Activity {
+public class Lembrador {
 	
-	private Lembrador lembrador;
-
-	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        lembrador = new Lembrador();
-        
-        Button cadastro = (Button) findViewById(R.id.cadastroAtividade);
-        cadastro.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				Intent telaAtividade = new Intent(MainActivity.this, CadastroDeAtividade.class);
-				MainActivity.this.startActivity(telaAtividade);
-				
-			}
-		});
-		
-		Button relatorio = (Button) findViewById(R.id.botaoRelatorioAtividade);
-		relatorio.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				
-				Intent telaRelatorio = new Intent(MainActivity.this, Historico.class);
-				MainActivity.this.startActivity(telaRelatorio);
-				
-			}
-		});
-		
-        
-        updateTimeAndDate();
-    }
+	private Date today;
+	private GerarRelatorioDaSemana relatorio;
 	
-	private void updateTimeAndDate() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
-		
-		sdf.setTimeZone(TimeZone.getDefault());
-		String currentDateandTime = sdf.format(new Date());
-		
-		int dia = Integer.parseInt(currentDateandTime.substring(6, 8));
-		int mes = Integer.parseInt(currentDateandTime.substring(4, 6));
-		int ano = Integer.parseInt(currentDateandTime.substring(0, 4));
-		
-		lembrador.updateDate(dia, mes, ano);
-		
-		//showMessage(String.valueOf(lembrador.getData().getDay()));
-	}
-	
-	private void showMessage(String msg){
-		AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-		alertDialog.setTitle(msg);
-		alertDialog.show();
+	public Lembrador(Context context) {
+		relatorio = new GerarRelatorioDaSemana(context);
 		
 	}
 	
+	public boolean wasAtividadeYesterday(String now){
+		Date data = new Date(2014, Mes.JUN.ordinal(), 17);
+		data.getDay();
+		return false;
+	}
+	
+	public void updateDate(int dia, int mes, int ano){
+		Date today = new Date(ano, mes -1, dia - 1);
+		this.today = today;
+	}
+	
+	public Date getData(){
+		return today;
+	}
+	
+	public List<Atividade> getTeste(){
+		System.out.println(">>>>");
+		System.out.println(relatorio == null);
+
+		return relatorio.getAtividadesOrdenadasDecrescente();
+	}
+	
+	public String getTesteString(){
+
+		return relatorio.getTeste();
+	}
+
 }
